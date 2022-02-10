9cfa17535ca256ed4653bdc238b4d2a03fc3f2c9
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2022-02-10 16:40:06 +0600

    feat: typeorm install & db connection

--- apps/task-management/src/app.module.ts
+++ apps/task-management/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from './environments/environment';
 import { TaskModule } from './task/task.module';
 
 export function response<T>(data: T, message: string) {
@@ -18,6 +20,19 @@ export function reject(message: string) {
 }
 
 @Module({
-    imports: [TaskModule],
+    imports: [
+        TaskModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: env.mysql.DB_HOST,
+            port: env.mysql.DB_PORT,
+            username: env.mysql.DB_USERNAME,
+            password: env.mysql.DB_PASSWORD,
+            database: env.mysql.DB_DATABASE,
+            autoLoadEntities: true,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+    ],
 })
 export class AppModule {}
--- apps/task-management/src/environments/environment.ts
+++ apps/task-management/src/environments/environment.ts
@@ -1,3 +1,25 @@
-export const environment = {
+export const env = {
     production: false,
+    mysql: {
+        DB_TYPE: 'mysql',
+        DB_HOST: 'localhost',
+        DB_PORT: 3306,
+        DB_USERNAME: 'root',
+        DB_PASSWORD: '',
+        DB_DATABASE: 'task_management',
+        DB_AUTO_LOAD_ENTITIES: true,
+        DB_ENTITIES: [__dirname + '/**/*.entity{.ts,.js}'],
+        DB_SYNCHRONIZE: true,
+    },
+    postgresql: {
+        DB_TYPE: 'postgresql',
+        DB_HOST: 'localhost',
+        DB_PORT: 5432,
+        DB_USERNAME: 'postgres',
+        DB_PASSWORD: 'postgres',
+        DB_DATABASE: 'task_management',
+        DB_AUTO_LOAD_ENTITIES: true,
+        DB_ENTITIES: [__dirname + '/**/*.entity{.ts,.js}'],
+        DB_SYNCHRONIZE: true,
+    },
 };
